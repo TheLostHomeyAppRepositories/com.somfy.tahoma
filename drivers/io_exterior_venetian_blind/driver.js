1cdd4da0892a094fb1a450f5332ac8d91af63186
AdyRock <38184396+AdyRock@users.noreply.github.com>
2023-04-01 23:00:15 +0100

    Added io:VenetianBlindIOComponent

--- drivers/io_exterior_venetian_blind/driver.js
+++ drivers/io_exterior_venetian_blind/driver.js
@@ -13,7 +13,7 @@ class ExteriorVenetianBlindDriver extends ioWindowCoveringsDriver
 
     async onInit()
     {
-        this.deviceType = ['io:ExteriorVenetianBlindIOComponent', 'ogp:VenetianBlind', 'io:ExteriorVenetianBlindUnoIOComponent'];
+        this.deviceType = ['io:ExteriorVenetianBlindIOComponent', 'ogp:VenetianBlind', 'io:ExteriorVenetianBlindUnoIOComponent', 'io:VenetianBlindIOComponent'];
 
         this.tilt_changedTrigger = this.homey.flow.getDeviceTriggerCard('windowcoverings_tilt_changed');
 
