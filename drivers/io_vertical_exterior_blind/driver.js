223d28674f1f69898e0762d27daae27b519ac3f3
Adrian Rockall <38184396+AdyRock@users.noreply.github.com>
2024-02-27 22:37:02 +0000

    clean format

--- drivers/io_vertical_exterior_blind/driver.js
+++ drivers/io_vertical_exterior_blind/driver.js
@@ -8,11 +8,13 @@ const Driver = require('../Driver');
  * Driver class for vertical exterior blinds with the io:VerticalExteriorAwningIOComponent controllable name in TaHoma
  * @extends {Driver}
  */
-class VerticalExteriorBlindDriver extends Driver {
+class VerticalExteriorBlindDriver extends Driver
+{
 
-  async onInit() {
-	this.deviceType = ['io:VerticalExteriorAwningIOComponent', 'io:VerticalExteriorAwningVeluxIOComponent', 'io:VerticalExteriorAwningWithBatteryIOComponent'];
-  }
+	async onInit()
+	{
+		this.deviceType = ['io:VerticalExteriorAwningIOComponent', 'io:VerticalExteriorAwningVeluxIOComponent', 'io:VerticalExteriorAwningWithBatteryIOComponent'];
+	}
 
 }
 
