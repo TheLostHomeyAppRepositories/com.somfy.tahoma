bed7779901bf5ea1b40a511fa76fbdba625c0b4f
Adrian Rockall <38184396+AdyRock@users.noreply.github.com>
2024-12-15 20:54:41 +0000

    Bump version to 4.0.98; add 'my_position' capability to exterior venetian blinds and update changelog

--- drivers/io_exterior_venetian_blind/device.js
+++ drivers/io_exterior_venetian_blind/device.js
@@ -8,22 +8,48 @@ const WindowCoveringsDevice = require('../WindowCoveringsDevice');
  * Device class for exterior venetian blinds with the io:ExteriorVenetianBlindIOComponent controllable name in TaHoma
  * @extends {WindowCoveringsDevice}
  */
-class ExteriorVenetianBlindDevice extends WindowCoveringsDevice {
+class ExteriorVenetianBlindDevice extends WindowCoveringsDevice
+{
 
-	async onInit() {
-		if (this.hasCapability('lock_state')) {
+	async onInit()
+	{
+		if (this.hasCapability('lock_state'))
+		{
 			this.removeCapability('lock_state').catch(this.error);
 		}
 
-		if (!this.hasCapability('quick_open')) {
+		if (!this.hasCapability('quick_open'))
+		{
 			this.addCapability('quick_open').catch(this.error);
 		}
 
-		if (!this.hasCapability('windowcoverings_tilt_set')) {
+		if (!this.hasCapability('windowcoverings_tilt_set'))
+		{
 			this.addCapability('windowcoverings_tilt_set').catch(this.error);
 		}
 
 		await super.onInit();
+
+		const dd = this.getData();
+		let controllableName = '';
+		if (dd.controllableName)
+		{
+			controllableName = dd.controllableName.toString().toLowerCase();
+		}
+
+		if (controllableName === 'ogp:venetianblind')
+		{
+			if (!this.hasCapability('my_position'))
+			{
+				this.addCapability('my_position').catch(this.error);
+			}
+
+			this.myCommand = 'goToAlias'; // Name of the command to set the My position
+		}
+		else if (this.hasCapability('my_position'))
+		{
+			this.removeCapability('my_position').catch(this.error);
+		}
 	}
 
 }
